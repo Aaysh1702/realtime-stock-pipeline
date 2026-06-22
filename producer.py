6c1ab642cf1f5967f6e299aace041e1b49594cc3
Aryan Shukla <satyaryanshukla@gmail.com>
2026-06-22 17:33:19 +0000

    feat: implement real-time stock API producer and resolve Docker/Kafka IPv4 network bridging

---
+++ producer.py
@@ -0,0 +1,33 @@
+import time 
+import json
+import yfinance as yf
+from kafka import KafkaProducer
+from datetime import datetime
+
+# connection to Kafka
+producer = KafkaProducer(
+    bootstrap_servers =['127.0.0.1:9092'],
+    api_version = (3, 3, 1),
+    value_serializer = lambda v: json.dumps(v).encode('utf-8')
+    )
+print("Producer Started. Print Ctrl+c to Stop.")
+
+while True:
+    try:
+        data = yf.Ticker("AAPL").history(period="1d",interval="1m")
+        latest_row = data.iloc[-1]
+
+        packet ={
+            "ticker":"AAPL",
+            "price": latest_row['Close'],
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        } 
+
+        producer.send('stock-prices', value=packet)
+        print(f"Sent Data to Kafka:{packet}")
+
+        time.sleep(5)
+
+    except Exception as e:
+        print(f"An error occurred:{e}")
+        time.sleep(5)
